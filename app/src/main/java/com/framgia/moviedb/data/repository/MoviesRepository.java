ee64247c2927270f96985688993c407a90df99c4
lamnn96 <lamnn.dev@gmail.com>
2018-11-26 19:02:42 +0700

    refs Task #3682 [Home] Get and display data from TMDb on Home Screen

---
+++ app/src/main/java/com/framgia/moviedb/data/repository/MoviesRepository.java
@@ -0,0 +1,27 @@
+package com.framgia.moviedb.data.repository;
+
+import com.framgia.moviedb.data.source.MoviesDataSource;
+import com.framgia.moviedb.data.source.remote.MovieRemoteDataSource;
+
+public class MoviesRepository implements MoviesDataSource {
+    private static MoviesRepository sInstance;
+    private MoviesDataSource mDataSource;
+    private MoviesRepository(MoviesDataSource moviesDataSource) {
+        mDataSource = moviesDataSource;
+    }
+
+    public static MoviesRepository getInstance(MoviesDataSource moviesDataSource) {
+        if (sInstance == null) {
+            sInstance = new MoviesRepository(moviesDataSource);
+        }
+        return sInstance;
+    }
+
+    @Override public void getMovies(LoadMoviesCallback loadMoviesCallback) {
+        mDataSource.getMovies(loadMoviesCallback);
+    }
+
+    @Override public void getMovie(String movieId, GetMovieCallback getMovieCallback) {
+
+    }
+}
---
+++ app/src/main/java/com/framgia/moviedb/utils/MethodUtils.java
@@ -0,0 +1,5 @@
+package com.framgia.moviedb.utils;
+
+public class MethodUtils {
+    public static final String METHOD_GET = "GET";
+}
